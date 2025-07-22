dc527116a8b48c9138a9c4f6d86a764bb7d3ec0f
Smah-amen <samahamen20001@gmail.com>
2025-07-22 16:20:04 +0300

    finsh moodboard

--- src/components/Moodboard.jsx
+++ src/components/Moodboard.jsx
@@ -1,40 +1,46 @@
 // components/Moodboard.tsx
 export default function Moodboard() {
-    const images = [
-      { src: "/pic2.jpg", col: "col-span-2", row: "row-span-2" },
-      { src: "/pic3.jpg", col: "col-span-1", row: "row-span-1" },
-      { src: "/pic1.jpg", col: "col-span-3", row: "row-span-1" },
-      { src: "/pic4.jpg", col: "col-span-1", row: "row-span-2" },
-      { src: "/pic5.jpg", col: "col-span-1", row: "row-span-1" },
-      // { src: "/acss6.jpg", col: "col-span-1", row: "row-span-1" },
-      // { src: "/acss6.jpg", col: "col-span-1", row: "row-span-1" },
-      { src: "/pic2.jpg", col: "col-span-2", row: "row-span-2" },
+  const images = [
+    { src: "/pic2.jpg", col: "col-span-2", row: "row-span-2" },
+    { src: "/pic3.jpg", col: "col-span-1", row: "row-span-1" },
+    { src: "/pic7.jpg", col: "col-span-3", row: "row-span-1" },
+    { src: "/copy.jpg", col: "col-span-1", row: "row-span-2" },
+    { src: "/pic5.jpg", col: "col-span-1", row: "row-span-1" },
+    // { src: "/acss6.jpg", col: "col-span-1", row: "row-span-1" },
+    // { src: "/acss6.jpg", col: "col-span-1", row: "row-span-1" },
+    { src: "/pic6.jpg", col: "col-span-2", row: "row-span-2" },
 
-      { src: "/acss5.jpg", col: "col-span-2", row: "row-span-1" },
+    { src: "/pic9.jpg", col: "col-span-2", row: "row-span-1" },
 
-    
-      { src: "/acss6.jpg", col: "col-span-1", row: "row-span-1" },
-   
-    ];
-  
-    return (
-      <section className="bg-[#f4f0ed] py-16 px-4 text-center">
-        <h2 className="text-4xl font-bold mb-4">vintage</h2>
-        <p className="text-gray-600 max-w-xl mx-auto mb-10">
-        A website with calm, neutral colors that convey the brand's sentiments: elegance, reassurance, warmth, sophistication, and uniqueness.        </p>
-  
-        <div className="grid grid-cols-6 auto-rows-[120px] md:auto-rows-[180px] gap-4 max-w-6xl mx-auto">
-          {images.map((img, index) => (
-            <div key={index} className={`${img.col} ${img.row}`}>
-              <img
-                src={img.src}
-                alt={`mood-${index}`}
-                className="w-full h-full object-cover rounded-lg shadow-md"
-              />
-            </div>
-          ))}
-        </div>
-      </section>
-    );
-  }
-  
+    { src: "/acss6.jpg", col: "col-span-1", row: "row-span-1" },
+  ];
+
+  return (
+    <section className="bg-[#f4f0ed] py-16 px-4 text-center">
+      <h2
+        style={{ fontFamily: "Fleur De Leah , cursive" }}
+        className=" text-8xl font-fleur mb-4"
+      >
+        Because elegance is timeless
+      </h2>
+      <p
+        style={{ fontFamily: "Dancing Script, cursive" }}
+       className=" text-[#762342] max-w-xl text-3xl mx-auto mb-10">
+        Embark on an exceptional journey with our pieces inspired by the charm
+        of the past
+      </p>
+
+      <div className="grid grid-cols-6 auto-rows-[120px] md:auto-rows-[180px] gap-4 max-w-6xl mx-auto">
+        {images.map((img, index) => (
+          <div key={index} className={`${img.col} ${img.row}`}>
+            <img
+              src={img.src}
+              alt={`mood-${index}`}
+              className="w-full h-full object-cover rounded-lg shadow-md"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
