570d46c377789111df3fcc1e52f51af60e608a41
Smah-amen <samahamen20001@gmail.com>
2025-09-23 14:57:34 +0300

    refactor authentication setup to include clientId and clientSecret for GoogleProvider

--- src/app/provederAuth/nextAuth.js
+++ src/app/provederAuth/nextAuth.js
@@ -1,8 +1,26 @@
+import { SessionStore } from "next-auth/core/lib/cookie"
 import GoogleProvider from "next-auth/providers/google"
+import { getMaxAge } from "next/dist/server/image-optimizer"
 export const authOptions = {
   providers: [
-    GoogleProvider()
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      
+    })
     
 
   ],
+
+  session :{
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+jwt :{
+  // A secret to use for key generation (you should set this explicitly)
+},
+callbacks :{
+  // async signIn({ user, account, profile, email, credentials }) { return true },
+}
+
 }
